6a0811ab64f6dc3d92d0b49cde2026b81cfccf87
fuaokai <fuaokai>
2020-06-16 17:36:47 +0800

    整理提交

---
+++ my-study-custom/src/main/java/cn/fuaokai/study/CustomApplication.java
@@ -0,0 +1,12 @@
+package cn.fuaokai.study;
+
+/**
+ * Created by CustomApplication
+ *
+ * @author Aokai.Fu
+ * @version v1.0
+ * @apiNote CustomApplication
+ * @create 2020/6/10 16:59
+ */
+public class CustomApplication {
+}
