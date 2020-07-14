66877192b2e0e81c3a2243bdddc11fb8edf8a5d1
fuaokai <fuaokai>
2020-07-14 19:40:47 +0800

    123123123

---
+++ simple-study/src/main/java/com/fsr/elasticsearch/ElasticsearchApplication.java
@@ -0,0 +1,13 @@
+package com.fsr.elasticsearch;
+
+/**
+ * Created by ElasticsearchApplication
+ *
+ * @author Aokai.Fu
+ * @version v1.0
+ * @apiNote ElasticsearchApplication
+ * @create 2020/7/14 14:13
+ */
+public class ElasticsearchApplication {
+
+}
