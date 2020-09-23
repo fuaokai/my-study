d61ad4db7c0afd960c64544584af0b398f1009bd
fuaokai <fuaokai>
2020-09-23 10:22:55 +0800

    123123123

---
+++ simple-study/src/main/java/com/fsr/design/behavior/state/ProblemState.java
@@ -0,0 +1,12 @@
+package com.fsr.design.behavior.state;
+
+/**
+ * Created by ProblemState
+ *
+ * @author Aokai.Fu
+ * @version v1.0
+ * @apiNote ProblemState
+ * @create 2020/9/23 10:21
+ */
+public class ProblemState {
+}
